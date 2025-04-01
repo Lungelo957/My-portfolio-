2bf54a4b24dcc2187704423a712de3539189d7d1
Lungelo957 <lungelomahaye95@gmail.com>
2025-04-01 17:54:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website loaded successfully!");
 });
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website loaded successfully!");
+    hideLoader();
+});
+
+function showLoader() {
+    document.getElementById('loader').style.display = 'flex';
+}
+
+function hideLoader() {
+    document.getElementById('loader').style.display = 'none';
+}
+
+// Show loader when navigating to a new page
+window.addEventListener('beforeunload', function() {
+    showLoader();
+});
