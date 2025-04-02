e0d2a666cf0c854e3c679c1f569d8b7ea0e6a8c9
Lungelo957 <lungelomahaye95@gmail.com>
2025-04-02 18:56:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ function hideLoader() {
 window.addEventListener('beforeunload', function() {
     showLoader();
 });
+
+// for images in the home page
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        dots[i].classList.remove("active");
+    });
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+}
+
+function moveSlide(step) {
+    slideIndex += step;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+    if (slideIndex >= slides.length) slideIndex = 0;
+    showSlide(slideIndex);
+}
+
+function currentSlide(index) {
+    slideIndex = index - 1;
+    showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
